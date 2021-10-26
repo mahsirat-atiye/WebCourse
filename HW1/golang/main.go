a1b7a628c55c084602025d42a2d8e55b23718460
Mohamad Hossein Alahdadian <alahdadian@viratech.ir>
2021-10-26 22:30:12 +0330

    HW1: GoLang: handle full logic and tested

--- HW1/golang/main.go
+++ HW1/golang/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"context"
-	// "fmt"
+	"crypto/sha256"
+	"encoding/hex"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
@@ -10,8 +11,11 @@ import (
 
 var ctx = context.Background()
 
+type Body struct {
+	Message string `form:"message" json:"message" binding:"required"`
+}
+
 func getClient() *redis.Client {
-	// connStr := ""
 	res := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "",
@@ -26,12 +30,13 @@ func main() {
 	r.GET("/sha256", func(c *gin.Context) {
 		id := c.Query("id")
 		client := getClient()
-		val, err := client.Get(ctx, id).Result()
+		message, err := client.Get(ctx, id).Result()
 		if err == redis.Nil {
 			c.JSON(404, gin.H{
 				"status": "not found",
-				id:       id,
+				"id":     id,
 			})
+			return
 		} else if err != nil {
 			c.JSON(500, gin.H{
 				"status": "error",
@@ -40,14 +45,24 @@ func main() {
 			panic(err)
 		}
 		c.JSON(200, gin.H{
-			"status": "getted",
-			id:       id,
-			val:      val,
+			"status":  "getted",
+			"id":      id,
+			"message": message,
 		})
 	})
 	r.POST("/sha256", func(c *gin.Context) {
-		id := c.Param("id")
-		message := c.PostForm("message")
+		var json Body
+		c.BindJSON(&json)
+		message := json.Message
+		if len(message) < 8 {
+			c.JSON(400, gin.H{
+				"status": "error",
+				"error":  "message was too short",
+			})
+			panic("message was too short")
+		}
+		sha := sha256.Sum256([]byte(message))
+		id := hex.EncodeToString(sha[:])
 		client := getClient()
 		err := client.Set(ctx, id, message, 0).Err()
 		if err != nil {
@@ -59,6 +74,7 @@ func main() {
 		}
 		c.JSON(200, gin.H{
 			"status":  "posted",
+			"id":      id,
 			"message": message,
 		})
 	})
