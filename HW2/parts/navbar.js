2ad6ae7091499afe3d5219ebdb50cda72c6dfaae
Mohamad Hossein Alahdadian <alahdadian@viratech.ir>
2021-11-14 21:58:09 +0330

    HW2: init: handle code intellisense for string templates

--- HW2/parts/navbar.js
+++ HW2/parts/navbar.js
@@ -1,8 +1,17 @@
 const element = document.getElementById("navbar");
 
-element.innerHTML = `
-<div>
-salam
-</div>
+const style = css`
+  .bootstap {
+    background: blue;
+  }
+`;
 
+element.innerHTML = html`
+  ${style}
+  <div class="">
+    <div class="bootstap">
+      adsds adsds adsds adsds adsds adsds adsds adsds adsds adsds adsds adsds
+    </div>
+    ${"2   salam"}
+  </div>
 `;
