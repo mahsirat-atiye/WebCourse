aa79797d33a3780b5548e0302f0ce1155e5dc008
Mohamad Hossein Alahdadian <alahdadian@viratech.ir>
2021-12-07 00:27:11 +0330

    HW2: fix responsivve of comments 2

--- HW2/parts/comments.js
+++ HW2/parts/comments.js
@@ -1,16 +1,9 @@
 const element = document.getElementById("comments");
 
 const style = css`
-  .container {
-    margin: auto;
-    padding: 10px;
-    cursor: pointer;
-    float: centre;
+  #comments {
     display: flex;
-    flex-direction: column;
     justify-content: center;
-    transition: all 2s;
-    position: relative;
   }
 
   .control {
@@ -51,40 +44,41 @@ const lorem =
   "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است ";
 element.innerHTML = html`
   ${style}
-  <div class="mb-3">
-    <div id="comments-crasual" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        ${["assets/commenter1.jpeg", "assets/commenter2.jpeg"].map(
-          (src, index) => html`
-            <div class="carousel-item ${index === 0 ? "active" : ""}">
-              <!-- <div class="card mb-3" style="max-width: 500px;"> -->
-              <div class="flex items-center">
-                <img src="${src}" class="img-fluid rounded-start" alt="..." />
-                <div class="card-body">
-                  <p>${lorem}</p>
-                </div>
+  <div
+    id="comments-crasual"
+    class="carousel slide"
+    data-bs-ride="carousel"
+    style="max-width: 500px;"
+  >
+    <div class="carousel-inner">
+      ${["assets/commenter1.jpeg", "assets/commenter2.jpeg"].map(
+        (src, index) => html`
+          <div class="carousel-item ${index === 0 ? "active" : ""}">
+            <div class="flex items-center">
+              <img src="${src}" class="img-fluid rounded-start" alt="..." />
+              <div class="card-body">
+                <p>${lorem}</p>
               </div>
-              <!-- </div> -->
             </div>
-          `
-        )}
-      </div>
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-bs-target="#comments-crasual"
-        data-bs-slide="prev"
-      >
-        <span class="control prev "></span>
-      </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-bs-target="#comments-crasual"
-        data-bs-slide="next"
-      >
-        <span class="control next "></span>
-      </button>
+          </div>
+        `
+      )}
     </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#comments-crasual"
+      data-bs-slide="prev"
+    >
+      <span class="control prev "></span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#comments-crasual"
+      data-bs-slide="next"
+    >
+      <span class="control next "></span>
+    </button>
   </div>
 `;
